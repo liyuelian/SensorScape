7c6d370026261aef4d6826d4c70eec22b2121936
liyuelian <liyuelian.ke@outlook.com>
2025-03-16 00:04:05 +0800

    项目初始化

---
+++ src/main/java/com/sensca/SensorScapeApplication.java
@@ -0,0 +1,13 @@
+package com.sensca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorScapeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SensorScapeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sensca/SensorScapeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sensca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorScapeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
